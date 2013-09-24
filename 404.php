6a8de64ade2ab420c92da66e2be5f31877f12866
Dustin Gary <dustin@putyourselfon.com>
2013-09-23 22:39:12 -0400

    took 404 redirect off

--- 404.php
+++ 404.php
@@ -1,1 +1,14 @@
-<?php header( 'Location: http://gossiponthis.com' ); ?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+<?php get_template_part('parts/shared/article_slider'); ?>
+<div class="row" id="bg_wrapper">
+	<div class="sixteen columns" id="primary">
+			<div class="row">
+				<div class="twentyfour columns">
+					<h2>404: Sorry, wrong URL!</h2>
+					<h4><a href="http://gossiponthis.com">CHECK OUT OUR HOMEPAGE FOR SOMETHING ELSE</a></h4>
+				</div>
+			</div>
+	</div>
+	<?php get_sidebar(); ?>
+</div>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
